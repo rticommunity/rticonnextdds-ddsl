1b32bf656fbe0b065309229642d490732fc47156
Rajive Joshi <rajive@rti.com>
2015-12-11 16:26:24 -0800

    testing: fixed a typo in the file name

--- test/ddsl-xtypes-tester.lua
+++ test/ddsl-xtypes-tester.lua
@@ -1963,7 +1963,7 @@ function Tester:test_xml_advanced()
     'xml-test-connector.xml',
     'xml-test-ddsc-types1.xml',
     'xml-test-union-enum.xml',
-    '../tutorial/types-lua.xml',
+    '../tutorial/json.xml',
   }
   
   -- xml.log.verbosity(xml.log.TRACE)
